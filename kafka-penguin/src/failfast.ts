ccf58852a75ec006c0ae9cce84397d8704560ec8
Kushal Talele <kushal.talele@gmail.com>
2021-04-07 14:08:32 -0400

    DLQ work

--- kafka-penguin/src/failfast.ts
+++ kafka-penguin/src/failfast.ts
@@ -4,8 +4,11 @@ interface messageValue {
 }
 class FailFast {
   retry: number;
-  client: any; 
+
+  client: any;
+
   innerProducer: any;
+
   constructor(num: number, kafkaJSClient: any) {
     this.retry = num;
     this.client = kafkaJSClient;
@@ -15,29 +18,30 @@ class FailFast {
   producer() {
     const options = {
       retry:
-        { retries: this.retry }
-    }
+        { retries: this.retry },
+    };
     // Create a producer from client passing in retry options
     // Save to FailFast class
     this.innerProducer = this.client.producer(options);
     // Return curr FailFast instance instead of a producer
     return this;
-
   }
 
-  connect() { 
-    return this.innerProducer.connect()
+  connect() {
+    return this.innerProducer.connect();
   }
+
   disconnect() {
-    return this.innerProducer.disconnect()
+    return this.innerProducer.disconnect();
   }
+
   send(message: messageValue) {
     return this.innerProducer.send(message)
       .catch((e: { retryCount?: any }) => {
         console.log(`FailFast stopped producer after ${this.retry + 1} times!`);
         this.innerProducer.disconnect();
-      })
-  } 
+      });
+  }
 }
 
 module.exports = {
